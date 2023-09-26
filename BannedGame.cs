9196cf66c71f35c371b0217f31c7a30b261a09d6
KyHOan <91080420+KyHOan@users.noreply.github.com>
2023-09-26 15:30:15 -0400

    Add files via upload

---
+++ BannedGame.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Query_Builder
+{
+    internal class BannedGame : IClassModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Series { get; set; }
+        public string Country { get; set; }
+        public string? Details { get; set; }
+
+        public BannedGame() { }
+        public BannedGame(int id, string title, string series, string country, string? details)
+        {
+            Id = id;
+            Title = title;
+            Series = series;
+            Country = country;
+            Details = details;
+        }
+
+        public override string ToString()
+        {
+            string msg = "";
+
+            msg += $"Id: {Id}\n";
+            msg += $"Title: {Title}\n";
+            msg += $"Series: {Series} \n";
+            msg += $"Country: {Country} \n";
+            msg += $"Details: {Details} \n";
+
+            return msg;
+        }
+    }
+}
+
---
+++ Pokemon.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Query_Builder
+{
+    internal class Pokemon : IClassModel
+    {
+        public int Id { get; set; }
+        public int DexNumber { get; set; }
+        public string Name { get; set; }
+        public string? Form { get; set; }
+        public string Type1 { get; set; }
+        public string? Type2 { get; set; }
+        public int Total { get; set; }
+        public int HP { get; set; }
+        public int Attack { get; set; }
+        public int Defense { get; set; }
+        public int SpecialAttack { get; set; }
+        public int SpecialDefense { get; set; }
+        public int Speed { get; set; }
+        public int Generation { get; set; }
+
+        public Pokemon() { }
+        public Pokemon(int id, int dexNumber, string name, string? form, string type1,
+        string type2, int total, int hp, int attack, int defense, int specialAttack,
+        int specialDefense, int speed, int generation)
+        {
+            Id = id;
+            DexNumber = dexNumber;
+            Name = name;
+            Form = form;
+            Type1 = type1;
+            Type2 = type2;
+            Total = total;
+            HP = hp;
+            Attack = attack;
+            Defense = defense;
+            SpecialAttack = specialAttack;
+            SpecialDefense = specialDefense;
+            Speed = speed;
+            Generation = generation;
+        }
+
+        // tostring
+
+        public override string ToString()
+        {
+            string msg = "";
+
+            msg += $"Id: {Id}  \n";
+            msg += $"Dex Number: {DexNumber}  \n";
+            msg += $"Name: {Name}  \n";
+            msg += $"Form: {Form}  \n";
+            msg += $"Type 1: {Type1}  \n";
+            msg += $"Type 2: {Type2}  \n";
+            msg += $"Total: {Total}  \n";
+            msg += $"Hit Points: {HP}  \n";
+            msg += $"Attack: {Attack}  \n";
+            msg += $"Defense: {Defense}  \n";
+            msg += $"Special Attack: {SpecialAttack}  \n";
+            msg += $"Special Defense: {SpecialDefense}  \n";
+            msg += $"Speed: {Speed}  \n";
+            msg += $"Generation: {Generation}  \n";
+
+            return msg;
+        }
+    }
+
+}
